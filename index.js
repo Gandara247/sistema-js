ba7b5866610f8e230dfb456ef4c10aa3a77694b3
Gandara <gustavogandarasilva@gmail.com>
2023-01-30 10:14:39 -0300

    add nova receita

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 import receitasTextos from './src/common/receitas-textos.js'
-
+import crypto from 'node:crypto';
+import { criarReceita } from './src/common/utils/criar-receitas.js';
+import { exibirReceitas } from './src/common/utils/exibir-receitas.js';
+const uuid = crypto.randomUUID();
 const nomeEmpresa = 'Gama';
-console.log(receitasTextos);
-// var receita = [{
-//     id:'',
-//     titulo:'',
-//     nivelDificuldade:'',
-//     ingredientes:'',
-//     instrucoes:'',
-//     linkVideo: 'https://www.youtube.com/watch?v=Mxc3RQE4yAg',
-//     vegana: false,
-// }]
+
+global.receitas = [{
+    id:uuid,
+    titulo:'Risoto de Abobora',
+    nivelDificuldade:'simples',
+    ingredientes: receitasTextos.ingredientesRisoto,
+    instrucoes: receitasTextos.modoPreparoRisoto,
+    linkVideo: 'https://www.youtube.com/watch?v=Mxc3RQE4yAg',
+    vegana: false,
+}]
+
+console.log(`***************${nomeEmpresa}*****************`);
+console.log('Bem vind@ a nossa página de receitas');
+criarReceita({
+    titulo:'Pão de queiojo',
+    nivelDificuldade:'simples',
+    ingredientes: '',
+    instrucoes: '',
+    linkVideo: '',
+    vegana: false,
+});
+
+exibirReceitas();
+console.log(receitas);
---
+++ src/common/utils/exibir-receitas.js
@@ -0,0 +1,14 @@
+function exibirReceitas(){
+    const receitasDesconstruidas = [];
+    global.receitas.map(item =>{
+        const {titulo, ingredientes, vegana} = item;
+        receitasDesconstruidas.push({
+            titulo,
+            ingredientes,
+            vegana,
+        })
+    })
+    console.log(receitasDesconstruidas);
+}
+
+export {exibirReceitas}
