c316563e7bde9fd44245b8693495ef40f4bc8c12
Gandara <gustavogandarasilva@gmail.com>
2023-01-29 23:34:30 -0300

    add package.json

---
+++ index.js
@@ -0,0 +1,13 @@
+import receitasTextos from './src/common/receitas-textos.js'
+
+const nomeEmpresa = 'Gama';
+console.log(receitasTextos);
+// var receita = [{
+//     id:'',
+//     titulo:'',
+//     nivelDificuldade:'',
+//     ingredientes:'',
+//     instrucoes:'',
+//     linkVideo: 'https://www.youtube.com/watch?v=Mxc3RQE4yAg',
+//     vegana: false,
+// }]
---
+++ src/common/receitas-textos.js
@@ -0,0 +1,5 @@
+const receitasTextos = {
+    fake: 'Descasque a abóbora e corte-a em cubinhos pequenos. Reserve. Coloque o azeite em uma panela funda e acrescente a cebola e o alho picados e leve ao fogo baixo até que a cebola e o alho murchem, mexendo sem parar. Adicione a cebolinha verde e a abóbora picada e deixe refogar por uns 3 minutos, mexendo sempre. Adicione o vinho branco e deixe cozinhar por uns 2 minutos. Adicione 2 xícaras de chá de água fervente e os tabletes de caldo de galinha e deixe cozinhar para que a abóbora fique ligeiramente macia, por uns 10 minutos em fogo baixo. Aumente o fogo e acrescente o arroz arbóreo. Mexa bem e deixe levantar fervura. Acrescente as ervas finas, o gengibre e a folha de louro. Diminua a chama do fogão e sempre mexendo vá acrescentando água fervente quanto necessário. Leva em média de 15 a 20 minutos para adquirir o ponto cremoso. Quanto estive al dente acrescente a manteiga e o queijo parmesão ralado. Experimente o sal e acrescente pimenta a gosto. Desligue o fogo e tampe a panela. Deixe descansar por uns 5 minutos. Sirva regado com queijo parmesão ralado e salsinha picada.',
+}
+
+export default receitasTextos
